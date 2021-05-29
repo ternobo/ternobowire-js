2817f214dd2f78788b7e64630b79e56e4ccf1d9d
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-05-29 16:54:50 +0430

    splash screen updated

--- dist/wire-js.esm.js
+++ dist/wire-js.esm.js
@@ -66,32 +66,6 @@ var WireLink = {
 //
 var script = {
   name: "AppLayout",
-
-  data() {
-    return {
-      loadingWidth: 0,
-      loadingInterval: null
-    };
-  },
-
-  watch: {
-    loading() {
-      if (!this.loading) {
-        clearInterval(this.loadingInterval);
-        this.loadingWidth = 100;
-      }
-    }
-
-  },
-
-  mounted() {
-    this.loadingInterval = setInterval(() => {
-      if (this.loadingWidth < 80) {
-        this.loadingWidth += 10;
-      }
-    }, 200);
-  },
-
   props: ["loading"]
 };
 
@@ -262,27 +236,35 @@ var __vue_render__ = function () {
     attrs: {
       "d": "M106.3 164.8l.3 89.7 2.6 10.6c3.3 13.5 9.5 29.8 15.2 40.2 17.1 31.5 45.8 57.5 78.5 71.2 8.6 3.6 21.6 7.8 28.5 9l3.6.7V277h-65v-2.3c0-1.2.6-4.5 1.4-7.2 3.9-14.8 13.9-25.6 28-30.5 4.9-1.7 8.4-2 20.9-2h15l-.6-12.5c-2-39.7-17.4-74.3-45.7-102.6-21.9-22-48.3-36.9-76.8-43.3l-6.3-1.4.4 89.6zM395.2 128.6c-21.8 6.4-38.3 14.6-55.4 27.4-18 13.5-33.5 31.7-43.7 51.1-5.2 9.9-11.1 23.9-11.1 26.4 0 1.3 4.1 1.5 28.6 1.5h28.6l-.7 4.7c-2.2 15-12.2 28.3-25.6 34.2-6.1 2.7-15.5 4.1-28.1 4.1H277v158.9l3.3-.6c23.8-4.5 52.5-19.1 73.3-37.2 25.6-22.2 44.6-56.5 50.5-90.9 1.8-10.7 1.9-16.8 1.9-96.8 0-67.7-.3-85.4-1.2-85.3-.7 0-5 1.1-9.6 2.5z"
     }
-  })]), _vm._v(" "), _c('div', {
+  })]), _vm._v(" "), _vm._m(0)]) : _vm._t("default")], 2);
+};
+
+var __vue_staticRenderFns__ = [function () {
+  var _vm = this;
+
+  var _h = _vm.$createElement;
+
+  var _c = _vm._self._c || _h;
+
+  return _c('div', {
     staticClass: "spinner",
     attrs: {
       "role": "spinner"
     }
   }, [_c('div', {
-    ref: "spinner",
-    staticClass: "spinner-icon",
-    style: {
-      width: _vm.loadingWidth + "%"
-    }
-  })])]) : _vm._t("default")], 2);
-};
-
-var __vue_staticRenderFns__ = [];
+    staticClass: "spinner-icon"
+  })]);
+}];
 /* style */
 
 const __vue_inject_styles__ = function (inject) {
   if (!inject) return;
-  inject("data-v-5d3e75a1_0", {
-    source: ".ternobo-loading[data-v-5d3e75a1]{display:flex;height:100vh;width:100vw;justify-content:center;align-items:center}.loading-container[data-v-5d3e75a1]{display:flex;flex-direction:column;justify-content:center;align-items:center}.spinner[data-v-5d3e75a1]{width:212px;height:5px;direction:ltr;background:#fff;margin-top:40px;box-shadow:0 6px 20px rgba(0,0,0,.2)}.spinner .spinner-icon[data-v-5d3e75a1]{background:#191919;height:100%;transition:.7s all;border-radius:5px;width:0}",
+  inject("data-v-6b078a9a_0", {
+    source: "@-webkit-keyframes spinner-indicating{0%{transform:translateX(0);width:80px}50%{transform:translateX(190px);width:0}to{transform:translateX(0)}}@keyframes spinner-indicating{0%{transform:translateX(0);width:80px}50%{transform:translateX(190px)}100%{transform:translateX(-40px)}}",
+    map: undefined,
+    media: undefined
+  }), inject("data-v-6b078a9a_1", {
+    source: ".ternobo-loading[data-v-6b078a9a]{display:flex;height:100vh;width:100vw;justify-content:center;align-items:center}.loading-container[data-v-6b078a9a]{display:flex;flex-direction:column;justify-content:center;align-items:center}.spinner[data-v-6b078a9a]{width:212px;height:5px;direction:ltr;background:#fff;margin-top:40px;box-shadow:0 6px 20px rgba(0,0,0,.2);overflow:hidden}.spinner .spinner-icon[data-v-6b078a9a]{background:#191919;height:100%;transition:.7s all;border-radius:5px;width:80px;animation-name:spinner-indicating;animation-duration:2s;animation-timing-function:cubic-bezier(0,0,.2,1);animation-iteration-count:infinite}",
     map: undefined,
     media: undefined
   });
@@ -290,7 +272,7 @@ const __vue_inject_styles__ = function (inject) {
 /* scoped */
 
 
-const __vue_scope_id__ = "data-v-5d3e75a1";
+const __vue_scope_id__ = "data-v-6b078a9a";
 /* module identifier */
 
 const __vue_module_identifier__ = undefined;
--- dist/wire-js.ssr.js
+++ dist/wire-js.ssr.js
@@ -129,29 +129,6 @@ var WireLink = {
 //
 var script = {
   name: "AppLayout",
-  data: function data() {
-    return {
-      loadingWidth: 0,
-      loadingInterval: null
-    };
-  },
-  watch: {
-    loading: function loading() {
-      if (!this.loading) {
-        clearInterval(this.loadingInterval);
-        this.loadingWidth = 100;
-      }
-    }
-  },
-  mounted: function mounted() {
-    var _this = this;
-
-    this.loadingInterval = setInterval(function () {
-      if (_this.loadingWidth < 80) {
-        _this.loadingWidth += 10;
-      }
-    }, 200);
-  },
   props: ["loading"]
 };function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
     if (typeof shadowMode !== 'boolean') {
@@ -284,9 +261,7 @@ var __vue_render__ = function __vue_render__() {
     attrs: {
       "id": "app"
     }
-  }, [_vm.loading ? _vm._ssrNode("<div class=\"loading-container\" data-v-5d3e75a1>", "</div>", [_vm._ssrNode("<svg version=\"1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"682.667\" height=\"682.667\" viewBox=\"0 0 512.000000 512.000000\" style=\"height: 64px; width: 64px\" data-v-5d3e75a1><path d=\"M106.3 164.8l.3 89.7 2.6 10.6c3.3 13.5 9.5 29.8 15.2 40.2 17.1 31.5 45.8 57.5 78.5 71.2 8.6 3.6 21.6 7.8 28.5 9l3.6.7V277h-65v-2.3c0-1.2.6-4.5 1.4-7.2 3.9-14.8 13.9-25.6 28-30.5 4.9-1.7 8.4-2 20.9-2h15l-.6-12.5c-2-39.7-17.4-74.3-45.7-102.6-21.9-22-48.3-36.9-76.8-43.3l-6.3-1.4.4 89.6zM395.2 128.6c-21.8 6.4-38.3 14.6-55.4 27.4-18 13.5-33.5 31.7-43.7 51.1-5.2 9.9-11.1 23.9-11.1 26.4 0 1.3 4.1 1.5 28.6 1.5h28.6l-.7 4.7c-2.2 15-12.2 28.3-25.6 34.2-6.1 2.7-15.5 4.1-28.1 4.1H277v158.9l3.3-.6c23.8-4.5 52.5-19.1 73.3-37.2 25.6-22.2 44.6-56.5 50.5-90.9 1.8-10.7 1.9-16.8 1.9-96.8 0-67.7-.3-85.4-1.2-85.3-.7 0-5 1.1-9.6 2.5z\" style=\"fill: #191919\" data-v-5d3e75a1></path></svg> <div role=\"spinner\" class=\"spinner\" data-v-5d3e75a1><div class=\"spinner-icon\"" + _vm._ssrStyle(null, {
-    width: _vm.loadingWidth + "%"
-  }, null) + " data-v-5d3e75a1></div></div>")], 2) : _vm._t("default")], 2);
+  }, [_vm.loading ? _vm._ssrNode("<div class=\"loading-container\" data-v-6b078a9a>", "</div>", [_vm._ssrNode("<svg version=\"1\" xmlns=\"http://www.w3.org/2000/svg\" width=\"682.667\" height=\"682.667\" viewBox=\"0 0 512.000000 512.000000\" style=\"height: 64px; width: 64px\" data-v-6b078a9a><path d=\"M106.3 164.8l.3 89.7 2.6 10.6c3.3 13.5 9.5 29.8 15.2 40.2 17.1 31.5 45.8 57.5 78.5 71.2 8.6 3.6 21.6 7.8 28.5 9l3.6.7V277h-65v-2.3c0-1.2.6-4.5 1.4-7.2 3.9-14.8 13.9-25.6 28-30.5 4.9-1.7 8.4-2 20.9-2h15l-.6-12.5c-2-39.7-17.4-74.3-45.7-102.6-21.9-22-48.3-36.9-76.8-43.3l-6.3-1.4.4 89.6zM395.2 128.6c-21.8 6.4-38.3 14.6-55.4 27.4-18 13.5-33.5 31.7-43.7 51.1-5.2 9.9-11.1 23.9-11.1 26.4 0 1.3 4.1 1.5 28.6 1.5h28.6l-.7 4.7c-2.2 15-12.2 28.3-25.6 34.2-6.1 2.7-15.5 4.1-28.1 4.1H277v158.9l3.3-.6c23.8-4.5 52.5-19.1 73.3-37.2 25.6-22.2 44.6-56.5 50.5-90.9 1.8-10.7 1.9-16.8 1.9-96.8 0-67.7-.3-85.4-1.2-85.3-.7 0-5 1.1-9.6 2.5z\" style=\"fill: #191919\" data-v-6b078a9a></path></svg> <div role=\"spinner\" class=\"spinner\" data-v-6b078a9a><div class=\"spinner-icon\" data-v-6b078a9a></div></div>")], 2) : _vm._t("default")], 2);
 };
 
 var __vue_staticRenderFns__ = [];
@@ -294,8 +269,12 @@ var __vue_staticRenderFns__ = [];
 
 var __vue_inject_styles__ = function __vue_inject_styles__(inject) {
   if (!inject) return;
-  inject("data-v-5d3e75a1_0", {
-    source: ".ternobo-loading[data-v-5d3e75a1]{display:flex;height:100vh;width:100vw;justify-content:center;align-items:center}.loading-container[data-v-5d3e75a1]{display:flex;flex-direction:column;justify-content:center;align-items:center}.spinner[data-v-5d3e75a1]{width:212px;height:5px;direction:ltr;background:#fff;margin-top:40px;box-shadow:0 6px 20px rgba(0,0,0,.2)}.spinner .spinner-icon[data-v-5d3e75a1]{background:#191919;height:100%;transition:.7s all;border-radius:5px;width:0}",
+  inject("data-v-6b078a9a_0", {
+    source: "@-webkit-keyframes spinner-indicating{0%{transform:translateX(0);width:80px}50%{transform:translateX(190px);width:0}to{transform:translateX(0)}}@keyframes spinner-indicating{0%{transform:translateX(0);width:80px}50%{transform:translateX(190px)}100%{transform:translateX(-40px)}}",
+    map: undefined,
+    media: undefined
+  }), inject("data-v-6b078a9a_1", {
+    source: ".ternobo-loading[data-v-6b078a9a]{display:flex;height:100vh;width:100vw;justify-content:center;align-items:center}.loading-container[data-v-6b078a9a]{display:flex;flex-direction:column;justify-content:center;align-items:center}.spinner[data-v-6b078a9a]{width:212px;height:5px;direction:ltr;background:#fff;margin-top:40px;box-shadow:0 6px 20px rgba(0,0,0,.2);overflow:hidden}.spinner .spinner-icon[data-v-6b078a9a]{background:#191919;height:100%;transition:.7s all;border-radius:5px;width:80px;animation-name:spinner-indicating;animation-duration:2s;animation-timing-function:cubic-bezier(0,0,.2,1);animation-iteration-count:infinite}",
     map: undefined,
     media: undefined
   });
@@ -303,10 +282,10 @@ var __vue_inject_styles__ = function __vue_inject_styles__(inject) {
 /* scoped */
 
 
-var __vue_scope_id__ = "data-v-5d3e75a1";
+var __vue_scope_id__ = "data-v-6b078a9a";
 /* module identifier */
 
-var __vue_module_identifier__ = "data-v-5d3e75a1";
+var __vue_module_identifier__ = "data-v-6b078a9a";
 /* functional template */
 
 var __vue_is_functional_template__ = false;
